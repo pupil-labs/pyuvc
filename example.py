2be869743ae63d46e40f3c2ca0711a119d58b6a9
Moritz Kassner <moritzkassner@gmail.com>
2015-05-19 16:19:01 +0200

    updated example.

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import uvc
 import logging
 import cv2
 from time import sleep
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 from time import time,sleep
 
 import numpy as np
@@ -16,26 +16,30 @@ dev_list =  uvc.device_list()
 print dev_list
 cap = uvc.Capture(dev_list[0]['uid'])
 # print cap.avaible_modes
-# cap.frame_mode = (640,480,120)
-# for x in range(10):
-# 	frame = cap.get_frame_robust()
-# 	cv2.imshow("img",frame.gray)
-# 	# cv2.imshow("u",u)
-# 	# cv2.imshow("v",v)
-# 	# sleep(.1)
-# 	cv2.waitKey(1)
-# 	# print frame.img.shape,x
-# 	frame = None
-# cap.frame_mode = (1280,720,30)
-# for x in range(10):
-# 	frame = cap.get_frame_robust()
-# 	cv2.imshow("img",frame.gray)
-# 	# cv2.imshow("u",u)
-# 	# cv2.imshow("v",v)
-# 	# sleep(.1)
-# 	cv2.waitKey(1)
-# 	# print frame.img.shape,x
-# 	frame = None
+cap.print_info()
+for x in range(10):
+	print x
+	# cap.frame_mode = (640,480,90)
+	cap.frame_mode = (640,480,30)
+	for x in range(3):
+		frame = cap.get_frame_robust()
+		frame.img
+		cv2.imshow("img",frame.gray)
+		# cv2.imshow("u",u)
+		# cv2.imshow("v",v)
+		# sleep(.1)
+		cv2.waitKey(1)
+		# print frame.img.shape,x
+	cap.frame_mode = (1280,720,30)
+	for x in range(3):
+		frame = cap.get_frame_robust()
+		frame.img
+		cv2.imshow("img",frame.gray)
+		# cv2.imshow("u",u)
+		# cv2.imshow("v",v)
+		# sleep(.1)
+		cv2.waitKey(1)
+		# print frame.img.shape,x
 
 cap = None
 exit()
