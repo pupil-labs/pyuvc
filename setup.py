6635828983b1ec9e7fbb13821a6b6a9e40c44cd9
andrewqandrew <ofandrewxia@gmail.com>
2015-06-10 12:04:42 +0200

    fixed build instructions for linux

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ if platform.system() == 'Darwin':
         tj_lib = '/usr/local/opt/jpeg-turbo/lib/libturbojpeg.a'
     except IndexError:
        raise Exception("Please install libturbojpeg")
+    include_dirs = ['/usr/local/opt/jpeg-turbo/include/']
 elif platform.system() == 'Linux':
     try:
         #check for tubo jpeg offical lib and select appropriate lib32/lib64 path.
@@ -30,6 +31,7 @@ elif platform.system() == 'Linux':
        raise Exception("Please install libturbojpeg")
     libs  = ['rt']
     extra_link_args = ['-Wl,-R/usr/local/lib/']
+    include_dirs = ['/opt/libjpeg-turbo/include']
 elif platform.system() == 'Windows':
     raise NotImplementedError("please fix me.")
 
@@ -37,7 +39,7 @@ elif platform.system() == 'Windows':
 extensions = [
     Extension(  name="uvc",
                 sources=['uvc.pyx'],
-                include_dirs =  [numpy.get_include(),'/usr/local/opt/jpeg-turbo/include/'],
+                include_dirs =  [numpy.get_include()]+include_dirs,
                 libraries = ['uvc',]+libs,
                 extra_link_args=extra_link_args,
                 extra_objects = [tj_lib],
