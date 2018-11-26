c6a8f8b5e85554341db5feeb8a85228d6c057da6
Pablo Prietz <pablo@prietz.org>
2018-11-26 18:47:49 +0100

    Apply black format

--- example.py
+++ example.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
 import uvc
 import logging
+
 logging.basicConfig(level=logging.INFO)
 
-dev_list =  uvc.device_list()
+dev_list = uvc.device_list()
 print(dev_list)
-cap = uvc.Capture(dev_list[0]['uid'])
+cap = uvc.Capture(dev_list[0]["uid"])
 
 # Uncomment the following lines to configure the Pupil 200Hz IR cameras:
 # controls_dict = dict([(c.display_name, c) for c in cap.controls])
@@ -15,10 +16,10 @@ cap = uvc.Capture(dev_list[0]['uid'])
 print(cap.avaible_modes)
 for x in range(10):
     print(x)
-    cap.frame_mode = (640,480,30)
+    cap.frame_mode = (640, 480, 30)
     for x in range(100):
         frame = cap.get_frame_robust()
         print(frame.img.shape)
-        #cv2.imshow("img",frame.gray)
-        #cv2.waitKey(1)
+        # cv2.imshow("img",frame.gray)
+        # cv2.waitKey(1)
 cap = None
