a1199d8a7ddad26e151682fdaf993d2a1714d1dc
Pablo Prietz <pablo@prietz.org>
2022-02-25 13:46:30 +0100

    Read package version using importlib

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,4 +3,3 @@ import pupil_labs.project_name as this_project
 
 def test_package_metadata() -> None:
     assert hasattr(this_project, "__version__")
-    assert hasattr(this_project, "__version_info__")
