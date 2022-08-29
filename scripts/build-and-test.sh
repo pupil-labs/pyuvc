e9b9abe2c77b41941abac4a83c5812271591e327
Pablo Prietz <pablo@prietz.org>
2022-08-29 13:55:01 +0200

    Fix sdist builds by correctly setting up rpath

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -3,13 +3,13 @@ git clean -dxf -e .venv/
 export PKG_CONFIG_PATH=/opt/homebrew/opt/jpeg-turbo/lib/pkgconfig
 
 BUILD_WHEEL=true
-if ["$BUILD_WHEEL" = true]
+if [ "$BUILD_WHEEL" = true ]
 then
     py -m build . --wheel
     delocate-wheel -v dist/*.whl
-    pip install dist/*.whl --force-reinstall
+    pip install -vv dist/*.whl --force-reinstall
 else
     py -m build . --sdist -n
-    pip install dist/*.tar.gz --force-reinstall
+    pip install -vv dist/*.tar.gz --force-reinstall
 fi
 python examples/access_100_frames.py
