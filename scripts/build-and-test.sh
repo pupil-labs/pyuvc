19247cbfcdfe859952b8c187f2bf4089c0a74388
Pablo Prietz <pablo@prietz.org>
2022-09-08 09:05:52 +0200

    Add example requirements

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -2,24 +2,24 @@
 
 if [ "$1" == "clean" ]
 then
-    git clean -dxf -e .venv/
+    git clean -dxf -e .venv/ -e .venv-dbg/
 fi
 
-export PKG_CONFIG_PATH=/opt/homebrew/opt/jpeg-turbo/lib/pkgconfig
+export PKG_CONFIG_PATH=/opt/homebrew/opt/jpeg-turbo/lib/pkgconfig:$PKG_CONFIG_PATH
 
 DIRECT_INSTALL=true
 BUILD_WHEEL=true
 
 if [ "$DIRECT_INSTALL" = true ]
 then
-    pip install -vv . --no-build-isolation
+    python -m pip install -v ".[example]" --no-build-isolation
 elif [ "$BUILD_WHEEL" = true ]
 then
     python -m build . --wheel -n
     delocate-wheel -v dist/*.whl
-    pip install -v dist/*.whl --force-reinstall
+    pip install -v "dist/*.whl[example]" --force-reinstall
 else
     python -m build . --sdist -n
-    pip install -vv dist/*.tar.gz --force-reinstall
+    pip install -vv "dist/*.tar.gz[example]" --force-reinstall
 fi
 python examples/access_100_frames.py
