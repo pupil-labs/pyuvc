b618263bd50309490c87dd0eeae10b62339b0956
Pablo Prietz <pablo@prietz.org>
2022-09-08 09:07:21 +0200

    Adjust debug examples

--- examples/access_100_frames.py
+++ examples/access_100_frames.py
@@ -1,21 +1,48 @@
-from __future__ import print_function
-import uvc
+#!python3
+
 import logging
+from rich import print
+from rich.logging import RichHandler
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.NOTSET,
+    handlers=[RichHandler(level="DEBUG")],
+    format="%(message)s",
+    datefmt="[%X]",
+)
 
-dev_list = uvc.device_list()
-print(dev_list)
-cap = uvc.Capture(dev_list[0]["uid"])
+import uvc
+
+
+for device in uvc.device_list():
+
+    cap = uvc.Capture(device["uid"])
+
+    for mode in cap.available_modes:
+        print(f"{cap.name} running at {mode}")
+        try:
+            cap.frame_mode = mode
+        except uvc.InitError as err:
+            print(f"{cap.name} mode selection - {err}")
+            continue
+        try:
+            for x in range(10):
+                frame = cap.get_frame_robust()
+                print("frame gray mean", frame.gray.mean())
+            # print(frame.img.mean())
+        except uvc.InitError as err:
+            print(f"{cap.name} getting frames - {err}")
+
+    cap.close()
+
 
 # Uncomment the following lines to configure the Pupil 200Hz IR cameras:
 # controls_dict = dict([(c.display_name, c) for c in cap.controls])
 # controls_dict['Auto Exposure Mode'].value = 1
 # controls_dict['Gamma'].value = 200
 
-print(cap.avaible_modes)
-cap.frame_mode = cap.avaible_modes[0]
-for x in range(100):
-    frame = cap.get_frame_robust()
-    print(frame.img.mean())
-cap = None
+# cap.frame_mode = cap.avaible_modes[0]
+# for x in range(100):
+#     frame = cap.get_frame_robust()
+#     print(frame.img.mean())
+# cap = None
---
+++ examples/vis_two_cameras.py
@@ -0,0 +1,97 @@
+import logging
+import os
+from typing import Iterable, NamedTuple, Optional
+
+import cv2
+import uvc.uvc_bindings as uvc
+from rich.logging import RichHandler
+from rich.traceback import install as install_rich_traceback
+
+
+class CameraSpec(NamedTuple):
+    name: str
+    width: int
+    height: int
+    fps: int
+    bandwidth_factor: float = 2.0
+
+
+def main(camera_specs: Iterable[CameraSpec]):
+    devices = uvc.device_list()
+    cameras = {spec: init_camera_from_list(devices, spec) for spec in camera_specs}
+    if not all(cameras.values()):
+        raise RuntimeError("Could not initialize all specified cameras")
+
+    try:
+        keep_running = True
+        while keep_running:
+            for spec, cam in cameras.items():
+                try:
+                    frame = cam.get_frame(timeout=0.005)
+                except TimeoutError:
+                    pass
+                except uvc.InitError as err:
+                    logging.debug(f"Failed to init {spec}: {err}")
+                    keep_running = False
+                    break
+                except uvc.StreamError as err:
+                    logging.debug(f"Failed to get a frame for {spec}: {err}")
+                else:
+                    cv2.imshow(
+                        spec.name, frame.bgr if hasattr(frame, "bgr") else frame.gray
+                    )
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+
+    except KeyboardInterrupt:
+        pass
+
+    for cam in cameras.values():
+        cam.close()
+
+
+def init_camera_from_list(devices, camera: CameraSpec) -> Optional[uvc.Capture]:
+    logging.debug(f"Searching {camera}...")
+    for device in devices:
+        if device["name"] == camera.name:
+            logging.debug(f"Found match by name")
+            capture = uvc.Capture(device["uid"])
+            capture.bandwidth_factor = camera.bandwidth_factor
+            for mode in capture.available_modes:
+                if mode[:3] == camera[1:4]:  # compare width, height, fps
+                    capture.frame_mode = mode
+                    return capture
+            else:
+                logging.warning(
+                    f"None of the available modes matched: {capture.available_modes}"
+                )
+            capture.close()
+    else:
+        logging.warning(f"No matching camera with name {camera.name!r} found")
+
+
+if __name__ == "__main__":
+    os.environ["LIBUSB_DEBUG"] = "2"
+    install_rich_traceback()
+    logging.basicConfig(
+        level=logging.NOTSET,
+        handlers=[RichHandler(level="DEBUG")],
+        format="%(message)s",
+        datefmt="[%X]",
+    )
+    # logging.getLogger("uvc").setLevel("INFO")
+    main(
+        [
+            CameraSpec("Redacted1", width=384, height=192, fps=200),
+            # CameraSpec(
+            #     "Redacted1",
+            #     #     width=1280,
+            #     #     height=720,
+            #     width=1600,
+            #     height=1200,
+            #     fps=30,
+            #     # bandwidth_factor=0.5,
+            #     bandwidth_factor=1.08,
+            # ),
+        ]
+    )
