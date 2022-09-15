bf450f1f404bedf02a8bb5c0bd33d71d6d5543e6
Pablo Prietz <pablo@prietz.org>
2022-09-15 15:48:13 +0200

    Build dependencies for manylinux

---
+++ scripts/build-deps-manylinux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+# Build libusb master
+git clone https://github.com/libusb/libusb.git /tmp/libusb
+pushd /tmp/libusb
+./bootstrap.sh
+./configure
+make -j
+make install
+popd
+
+# Build libturbojpeg
+pushd /tmp
+curl -L -o libjpeg-turbo.tar.gz https://sourceforge.net/projects/libjpeg-turbo/files/1.5.1/libjpeg-turbo-1.5.1.tar.gz/download
+tar xvzf libjpeg-turbo.tar.gz
+pushd libjpeg-turbo-1.5.1
+./configure --prefix=/usr/local
+make -j
+make install
+popd
+popd
