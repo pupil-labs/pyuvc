a50bbfac1dae9c7b8964743891e625a520e024de
Pablo Prietz <pablo@prietz.org>
2022-09-21 12:23:17 +0200

    Build custom libusb on Unix fixing https://github.com/libusb/libusb/issues/1199

--- scripts/build-deps-unix.sh
+++ scripts/build-deps-unix.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -xe
 
+# Build libusb master
+git clone --branch fix-1199 https://github.com/pupil-labs/libusb.git /tmp/libusb
+pushd /tmp/libusb
+./bootstrap.sh
+./configure
+make -j
+make install
+popd
+
 BUILD_DIR=libuvc-source/build
 
 rm -rf $BUILD_DIR
