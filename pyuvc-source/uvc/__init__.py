c0093ec61eca18f93bb72a67993ab6ebdecdd810
Pablo Prietz <pablo@prietz.org>
2022-11-11 12:49:58 +0100

    Import symbols from uvc_bindings explicitly

--- pyuvc-source/uvc/__init__.py
+++ pyuvc-source/uvc/__init__.py
@@ -5,14 +5,33 @@ try:
 except ImportError:
     from importlib_metadata import PackageNotFoundError, version
 
+from .uvc_bindings import (
+    Capture,
+    Device_List,
+    InitError,
+    OpenError,
+    StreamError,
+    device_list,
+    get_time_monotonic,
+    is_accessible,
+)
+
 try:
     __version__ = version("pupil-labs-uvc")
 except PackageNotFoundError:
     # package is not installed
     pass
 
-__all__ = ["__version__"]
-
 logger = logging.getLogger(__name__)
 
-from .uvc_bindings import *
+__all__ = [
+    "__version__",
+    "Capture",
+    "device_list",
+    "Device_List",
+    "get_time_monotonic",
+    "InitError",
+    "is_accessible",
+    "OpenError",
+    "StreamError",
+]
